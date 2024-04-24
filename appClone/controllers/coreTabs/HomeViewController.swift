ae70db2689452ef1f1a92aedb35bd027a3c05781
Lokesh Cheenu <lokeshcheenu@Lokeshs-MacBook-Pro.local>
2024-04-24 17:59:15 +0530

    first commit

---
+++ appClone/controllers/coreTabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appClone
+//
+//  Created by Lokesh Cheenu on 24/04/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
